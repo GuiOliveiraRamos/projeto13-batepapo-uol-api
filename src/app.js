a77d6eebc8ee897156f70511f90231d7a05d1954
GuiOliveiraRamos <guioliveiraramos30@gmail>
2023-06-27 09:36:52 -0300

    post /participants iniciado

--- src/app.js
+++ src/app.js
@@ -5,5 +5,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const participants = [];
+
+app.post("/participants", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.sendStatus(422);
+  }
+  if (participants.includes(name)) {
+    return res.sendStatus(409);
+  }
+  participants.push(name);
+  res.sendStatus(201);
+});
+
 const PORT = 5000;
 app.listen(PORT, `Servidor rodando na porta ${PORT}`);

fea663619bbd8bf1dfd946985082a10c86288a4a
GuiOliveiraRamos <guioliveiraramos30@gmail>
2023-06-28 09:55:25 -0300

    implementações db messages

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
+dayjs().format();
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
@@ -24,9 +26,9 @@ app.post("/participants", (req, res) => {
   if (!name || typeof name !== "string" || name.length === 0) {
     return res.sendStatus(422);
   }
-  if (req.body.includes(name)) {
-    return res.sendStatus(409);
-  }
+  // if (participants.includes(name)) {
+  //    return res.sendStatus(409);
+  // }
   const newParticipant = { name, lastStatus: Date.now() };
 
   const promise = db.collection("participants").insertOne(newParticipant);
@@ -37,45 +39,50 @@ app.post("/participants", (req, res) => {
 });
 
 app.get("/participants", (req, res) => {
-  if (participants.length === 0) {
-    res.send([]);
-  }
   const promise = db.collection("participants").find().toArray();
   promise.then((data) => res.send(data));
   promise.catch((err) => res.status(500).send(err.message));
 });
 
 app.post("/messages", (req, res) => {
-  const from = req.headers.from;
+  //  const from = req.headers.from;
   const { to, text, type } = req.body;
 
-  if (!to || typeof to === "string" || to.length === 0) {
-    return res.sendStatus(422);
-  }
-  if (!text || typeof text === "string" || text.length === 0) {
-    return res.sendStatus(422);
-  }
-  if (type !== message || type !== private_message) {
-    return res.sendStatus(422);
-  }
-  if (!from.includes(participants)) {
-    return res.sendStatus(422);
-  }
+  // if (!to || typeof to === "string" || to.length === 0) {
+  //  return res.sendStatus(422);
+  //}
+  //if (!text || typeof text === "string" || text.length === 0) {
+  //  return res.sendStatus(422);
+  //}
+  //if (type !== message || type !== private_message) {
+  //  return res.sendStatus(422);
+  // }
+  //  if (!from.includes(participants)) {
+  //    return res.sendStatus(422);
+  //  }
+  const newMessage = { to, text, type, time: dayjs().format("HH:mm:ss") };
+  const promise = db.collection("messages").insertOne(newMessage);
+
+  promise.then(() => res.status(201).send("Mensagem enviada!"));
+  promise.catch((err) => res.status(422).send(err.message));
   res.sendStatus(201);
 });
 
 app.get("/messages", (req, res) => {
-  const user = req.headers.user;
-  const limit = parseInt(req.query.limit);
-  if (isNaN(limit) || limit < 0) {
-    return res.sendStatus(422);
-  }
-  if (!limit) {
-    res.send(messages);
-  }
-  if (limit) {
-    res.send(messages.slice(-limit));
-  }
+  //  const user = req.headers.user;
+  //const limit = parseInt(req.query.limit);
+  //if (isNaN(limit) || limit < 0) {
+  //  return res.sendStatus(422);
+  //}
+  //  if (!limit) {
+  //    res.send(messages);
+  //  }
+  //  if (limit) {
+  //    res.send(messages.slice(-limit));
+  //  }
+  const promise = db.collection("messages").find().toArray();
+  promise.then((data) => res.send(data));
+  promise.catch((err) => res.status(500).send(err.message));
 });
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
